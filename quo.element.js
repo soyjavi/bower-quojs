8156a1641235489ed3a78a4d5924e6edd0891439
soyjavi <javi.jimenez.villar@gmail.com>
2015-04-27 09:43:46 +0700

    Release 3.0.7

--- quo.element.js
+++ quo.element.js
@@ -1,6 +1,6 @@
 /**
  * QuoJS - Micro #JavaScript Library for Mobile Devices.
- * @version v3.0.6
+ * @version v3.0.7
  * @link    http://quojs.tapquo.com
  * @author  Javi Jimenez Villar (@soyjavi) (https://twitter.com/soyjavi)
  * @license MIT
